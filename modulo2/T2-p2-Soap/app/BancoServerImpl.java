d97c356a4fbfc95d1900546d4e69b57e49a21a01
Jadley <jadleyd@gmail.com>
2022-12-10 19:37:37 -0300

    adicioando imcompleto, indo terminar no notebook

---
+++ modulo2/T2-p2-Soap/app/BancoServerImpl.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.util.Date;
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "app.BancoServer")
+public class BancoServerImpl implements bancoServer {
+
+    public String pegarBoleto(String codigoBarras) {
+        return "Boleto para o codigo" + codigoBarras;
+    }
+
+    public String criarBoleto(float valor, String cpfCliente) {
+        return "123.456.78910";
+    }
+
+    public boolean verificarPagameto(String codigoBarras) {
+        return true;
+    }
+
+    public boolean cancelarBoleto(String codigoBarras) {
+        return false;
+    }
+}
